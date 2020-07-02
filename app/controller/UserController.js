7240a50096b190f192d9be23b8fa78c84ed90d17
rvalessandro <ralessandro939@gmail.com>
2020-07-02 23:15:41 +0800

    Added user transactions endpoint

--- app/controller/UserController.js
+++ app/controller/UserController.js
@@ -1,4 +1,7 @@
 const User = require("../models/User");
+const Transaction = require("../models/Transaction");
+const Cart = require("../models/Cart");
+const Product = require("../models/Product");
 
 /**
  * @description Get all users
@@ -8,6 +11,26 @@ exports.getUsers = async (req, res, next) => {
   try {
     const users = await User.findAll({ attributes: ["id", "name", "email"] });
     res.json(users);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+};
+
+/**
+ * @description Get all user transactions
+ * @route       GET /api/users/:id/transactions
+ */
+exports.userTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.findAll({
+      where: { userId: req.params.id },
+      include: [
+        { model: User, attributes: ["id", "name", "email"] },
+        { model: Cart, include: [{ model: Product }] }
+      ]
+    });
+
+    res.json(transactions);
   } catch (err) {
     res.status(400).send(err.message);
   }
--- app/router/modules/users.js
+++ app/router/modules/users.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 
-const { getUsers } = require("../../controller/UserController");
+const {
+  getUsers,
+  userTransactions
+} = require("../../controller/UserController");
 
 router.route("/").get(getUsers);
+router.route("/:id/transactions").get(userTransactions);
 
 module.exports = router;
